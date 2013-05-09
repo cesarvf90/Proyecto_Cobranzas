e0c9166f50cdab10e26b477db427b42adb4d5c01
César Vásquez Flores <cesarvf90@gmail.com>
2013-05-09 12:05:03 -0500

    Administracion de usuarios y roles

---
+++ RecaudaSoft/Controllers/RolesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecaudaSoft.Models;
+
+namespace RecaudaSoft.Controllers
+{
+    public class RolesController : Controller
+    {
+        //
+        // GET: /Roles/
+
+        public ActionResult Index()
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Rols.ToList());
+            }
+        }
+
+        //
+        // GET: /Roles/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Roles/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Roles/Create
+
+        [HttpPost]
+        public ActionResult Create(Rol rol)
+        {
+            try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Rols.Add(rol);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Roles/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Rols.Find(id));
+            }
+        }
+
+        //
+        // POST: /Roles/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, Rol rol)
+        {
+            try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Entry(rol).State = System.Data.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Roles/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Rols.Find(id));
+            }
+        }
+
+        //
+        // POST: /Roles/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, Rol rol)
+        {
+            try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Entry(rol).State = System.Data.EntityState.Deleted;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RecaudaSoft/Controllers/UsuariosController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecaudaSoft.Models;
+
+namespace RecaudaSoft.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        //
+        // GET: /Usuarios/
+
+        public ActionResult Index()
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Usuarios.ToList());
+            }
+        }
+
+        //
+        // GET: /Usuarios/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Usuarios/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Usuarios/Create
+
+        [HttpPost]
+        public ActionResult Create(Usuario usuario)
+        {
+            try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Usuarios.Add(usuario);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Usuarios/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Usuarios.Find(id));
+            }
+        }
+
+        //
+        // POST: /Usuarios/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, Usuario usuario)
+        {
+            try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Entry(usuario).State = System.Data.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Usuarios/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Usuarios.Find(id));
+            }
+        }
+
+        //
+        // POST: /Usuarios/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, Usuario usuario)
+        {
+            try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Entry(usuario).State = System.Data.EntityState.Deleted;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
