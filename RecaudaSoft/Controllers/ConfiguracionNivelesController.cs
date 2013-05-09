6fef7db6695eeb24d5e37d54d814dca697228b1c
César Vásquez Flores <cesarvf90@gmail.com>
2013-05-09 10:52:35 -0500

    Maestro de Tipo de Actividad

---
+++ RecaudaSoft/Controllers/ConfiguracionNivelesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecaudaSoft.Controllers
+{
+    public class ConfiguracionNivelesController : Controller
+    {
+        //
+        // GET: /ConfiguracionNiveles/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /ConfiguracionNiveles/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /ConfiguracionNiveles/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /ConfiguracionNiveles/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /ConfiguracionNiveles/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ConfiguracionNiveles/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ConfiguracionNiveles/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ConfiguracionNiveles/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RecaudaSoft/Controllers/TiposActividadController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecaudaSoft.Models;
+
+namespace RecaudaSoft.Controllers
+{
+    public class TiposActividadController : Controller
+    {
+        //
+        // GET: /TiposActividad/
+
+        public ActionResult Index()
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Parametroes.ToList());
+            }
+        }
+
+        //
+        // GET: /TiposActividad/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /TiposActividad/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /TiposActividad/Create
+
+        [HttpPost]
+        public ActionResult Create(Parametro tipoActividad)
+        {
+            try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Parametroes.Add(tipoActividad);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /TiposActividad/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Parametroes.Find(id));
+            }
+        }
+
+        //
+        // POST: /TiposActividad/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, Parametro tipoActividad)
+            {
+            try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Entry(tipoActividad).State = System.Data.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /TiposActividad/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            using (var db = new CobranzasEntities())
+            {
+                return View(db.Parametroes.Find(id));
+            }
+        }
+
+        //
+        // POST: /TiposActividad/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, Parametro tipoActvidad)
+        {
+         try
+            {
+                using (var db = new CobranzasEntities())
+                {
+                    db.Entry(tipoActvidad).State = System.Data.EntityState.Deleted;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
