da20ca4e33d95c0592b316aa6f65e69ec81b20f5
César Vásquez Flores <cesarvf90@gmail.com>
2013-05-18 22:01:29 -0500

    Se implementa cascaron de pantallas de Registrar Actividaddes

--- RecaudaSoft/Controllers/RegistroAvanceController.cs
+++ RecaudaSoft/Controllers/RegistroAvanceController.cs
@@ -26,7 +26,38 @@ namespace RecaudaSoft.Controllers
 
         public ActionResult Details(int id)
         {
+            using (var db = new CobranzasEntities())
+            {
+                var listaDeudas = db.Deudas.Include("Parametro");
+                Deuda deuda = listaDeudas.First(a => a.idDeuda == id);
+
+                ViewBag.moneda = new SelectList(db.Parametroes.Where(p => p.tipo == "MONEDA"), "idParametro", "valor", deuda.moneda).ToList();
+
+                return View(deuda);
+            }
+        }
+        
+        //
+        // GET: /RegistroAvance/Edit/5
+
+        public ActionResult RegistrarActividad(int id)
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarActividad(int id, Actividad actividad)
+        {
+            try
+            {
+                // TODO Registrar la actividad en la BD
+                // Se le debe setear la deuda y el gestor a la actividad (se obtienen de la misma transaccion)
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         //
