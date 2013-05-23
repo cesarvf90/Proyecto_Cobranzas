171aab1dbb78a61d57f94c35daf1ad92004df3a0
César Vásquez Flores <cesarvf90@gmail.com>
2013-05-23 03:34:08 -0500

    Vista de asignacion de deudas

---
+++ RecaudaSoft/Controllers/AsignacionDeudasController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecaudaSoft.Models;
+using RecaudaSoft.Utils;
+
+namespace RecaudaSoft.Controllers
+{
+    public class AsignacionDeudasController : Controller
+    {
+                
+        //public ArrayList carteras = new ArrayList() { get; set; }
+        //public ArrayList gestores = new ArrayList() { get; set; }
+
+        //
+        // GET: /AsignacionDeudas/Index
+
+        public ActionResult Index()
+        {
+            using (var db = new CobranzasEntities())
+            {
+                ModeloAsignacion objetoModelo = new ModeloAsignacion();
+                objetoModelo.gestores = db.Gestors.Include("Parametro").Include("Parametro1").Include("Parametro2").ToList();
+                objetoModelo.carteras = db.Carteras.Include("Acreedor").Include("Parametro").ToList();
+                return View(objetoModelo);
+            }
+        }
+
+        //
+        // GET: /AsignacionDeudas/EleccionCarteras
+
+        public ActionResult EleccionCarteras()
+        {
+            using (var db = new CobranzasEntities())
+            {
+                var listaCarteras = db.Carteras.Include("Acreedor");
+                listaCarteras = listaCarteras.Include("Parametro");
+                return View(listaCarteras.ToList());
+            }
+        }
+    
+        //
+        // GET: /AsignacionDeudas/EleccionGestores
+
+        public ActionResult EleccionGestores()
+        {
+            using (var db = new CobranzasEntities())
+            {
+                var listaGestores = db.Gestors.Include("Parametro").Include("Parametro1").Include("Parametro2");
+                return View(listaGestores.ToList());
+            }
+        }
+    }
+}
